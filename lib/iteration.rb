6324077962b76bb35219655f9edbc0f18a0c5bbb
Lydia B <lydiabaik@gmail.com>
2019-12-08 08:08:41 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,63 @@
-def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+def join_ingredients(arr)
+  new_arr = []
+  row_index = 0
+  while row_index < arr.count do
+    element_index = 0
+    while element_index < arr[row_index].count-1 do
+      new_arr.push("I love #{arr[row_index][element_index]} and #{arr[row_index][element_index+1]} on my pizza")
+    element_index += 1
+    end
+  row_index += 1
 end
+  new_arr
+end
+
+#describe "Write a method called \"join_ingredients\"." do
+  #describe "It is given an Array of 2-element Arrays" do
+    #it "returns an Array of Strings of the form \"I love element1 and element2 on my pizza\"" do
+      # expect(join_ingredients(array_1)).to eq([
+      #  "I love pepperoni and sausage on my pizza",
+      #  "I love green olives and green peppers on my pizza",
+      # {}"I love onions and pineapple on my pizza"
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  new_arr = []
+  i = 0
+    while i < src.count do
+      new_arr.push(src[i].max)
+      i += 1
+    end
+  new_arr
 end
 
+
+# def find_greater_pair(src)
+#   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+#   # Produce a new Array that contains the larger number of each of the pairs
+#   # that are in the inner Arrays
+# end
+
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  total = 0
+  row_index = 0
+    while row_index < src.count do
+      element_index = 0
+      while element_index < src[row_index].count - 1 do
+        if src[row_index][element_index] % 2 == 0 && src[row_index][element_index + 1] % 2 == 0
+          total += src[row_index][element_index] + src[row_index][element_index + 1]
+        end
+        element_index += 1
+      end
+      row_index += 1
+    end
+  total
 end
+
+# def total_even_pairs(src)
+#   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+#   # if both numbers in the pair are even, then add both those numbers to the
+#   # total
+#   #
+#   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
+#   # the number was even. Review the operator documentation if you've forgotten
+# end
