3f5c48345edf52e72c2de5266b5d7de5aa5eb84e
Lydia B <lydiabaik@gmail.com>
2019-12-08 08:07:41 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,7 +42,7 @@ def total_even_pairs(src)
   row_index = 0
     while row_index < src.count do
       element_index = 0
-      while element_index < src[row_index].count-1 do
+      while element_index < src[row_index].count - 1 do
         if src[row_index][element_index] % 2 == 0 && src[row_index][element_index + 1] % 2 == 0
           total += src[row_index][element_index] + src[row_index][element_index + 1]
         end
